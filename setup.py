d6edd557637860c2f667e69ecdcf22d9ca0f43a6
Sangwon Lim <sangwonl@uvic.ca>
2022-05-30 18:09:53 -0600

    CPM_extractor

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+__version__ = (0, 0, 0)
+
+setup(
+    name="CPM_extractor",
+    description="Pyhton package to modify the CPM column in a kml file",
+    url="https://github.com/sum1lim/CPM_extractor",
+    version=".".join(str(d) for d in __version__),
+    author="Sangwon Lim",
+    author_email="sangwon3@ualberta.ca",
+    include_package_data=True,
+    scripts="""
+        ./scripts/CPM_extractor
+    """.split(),
+)
